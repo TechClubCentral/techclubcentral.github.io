ee6d87c510f7c9d9120d03d4543f582b46ea3b83
Beverly Nelson <bevcnelson@gmail.com>
2015-05-16 10:01:15 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+ar red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = "Tech Club";
+
+letterColors = [red, green, blue, orange];
+
+if(10 > 3) {
+    bubbleShape = "circle";
+}
+else {
+    bubbleShape = "square";
+}
+
+drawName(myName, letterColors);
+bounceName();
+bounceBubbles();
